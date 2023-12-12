f4c8e4c462f073fa147a47c18ddd35f52236f4ca
VvN123 <heero7_@hotmail.com>
2023-12-11 21:40:01 -0300

    1

--- src/app/paginas/detalle-clase/detalle-clase.page.ts
+++ src/app/paginas/detalle-clase/detalle-clase.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FirebaseService } from 'src/app/servicio/firebase.service';
+import { Clase } from 'src/app/modelo/clase';
 
 @Component({
   selector: 'app-detalle-clase',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetalleClasePage implements OnInit {
 
-  constructor() { }
+
+  asignatura : Clase = new Clase()
+  codigoAsignatura: string = '';
+
+  constructor(private activatedroute: ActivatedRoute, private fire:FirebaseService, private router : Router) { }
 
   ngOnInit() {
+    this.activatedroute.queryParams.subscribe(async params => {
+      if (params['codigo']) {
+        this.codigoAsignatura = params['codigo'];
+        const asignaturaObtenida = await this.fire.obtenerAsignatura(this.codigoAsignatura);
+        if (asignaturaObtenida) {
+          this.asignatura = asignaturaObtenida;
+        } else {
+          // Manejar el caso de que la asignatura no se encuentre
+          console.log('Asignatura no encontrada');
+        }
+      }
+    });
+  }
+
+  async crearAsistencia(){
+    const uuid = await this.fire.crearAsistencia(this.asignatura)
+    this.verDetalleAsignatura(uuid)
+
+  }
+
+  verDetalleAsignatura(id_asistencia: string) {
+    this.router.navigate(['/qr'], {
+      queryParams: { id_asistencia: id_asistencia }
+    });
   }
 
 }
--- src/app/paginas/home-alumno/home-alumno.page.ts
+++ src/app/paginas/home-alumno/home-alumno.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
+import { Usuario } from 'src/app/modelo/usuario';
+import { Clase } from 'src/app/modelo/clase';
+import { FirebaseService } from 'src/app/servicio/firebase.service';
+import { BarcodeScanner } from '@capacitor-mlkit/barcode-scanning';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-home-alumno',
@@ -7,9 +13,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeAlumnoPage implements OnInit {
 
-  constructor() { }
+  lecturaQr?: string
+  asignaturas: Clase[] = []; // Lista para almacenar las asignaturas
+  usuario: Usuario = new Usuario()
+
+  constructor(
+    private storage: Storage,
+    private fire: FirebaseService,
+    private changeDetect: ChangeDetectorRef) { }
 
   ngOnInit() {
+    this.cargarDatosUsuario()
   }
 
+
+  async cargarDatosUsuario() {
+    const usuario = await this.storage.get('currentUser');
+    if (usuario) {
+      this.usuario = usuario;
+      console.log(usuario);
+      await this.cargarAsignaturas(); // Cargar las asignaturas del usuario
+    } else {
+      console.log('No se encontraron datos del usuario');
+      // Manejar la situación cuando no hay datos del usuario
+    }
+  }
+
+  async cargarAsignaturas() {
+    for (const nombreAsignatura of this.usuario.clases_inscrita) {
+      try {
+        const asignatura = await this.fire.obtenerAsignatura(nombreAsignatura);
+        if (asignatura) {
+          this.asignaturas.push(asignatura);
+        } else {
+          console.log(`Asignatura no encontrada: ${nombreAsignatura}`);
+        }
+      } catch (error) {
+        console.error(`Error al cargar la asignatura ${nombreAsignatura}:`, error);
+      }
+    }
+    console.log('Asignaturas cargadas:', this.asignaturas);
+  }
+
+  async startScan() {
+    document.body.classList.add('barcode-scanner-active');
+    const listener = await BarcodeScanner.addListener('barcodeScanned', async (result) => {
+      if (result.barcode.displayValue) {
+        this.lecturaQr = result.barcode.displayValue;
+        console.log("COMPONENTE " + "|", this.lecturaQr, "|")
+        console.log("COMPONENTE " + this.usuario.rut)
+        this.stopScan();
+        await this.fire.alumnoPresente(this.lecturaQr, this.usuario.rut)
+      }
+    });
+    await BarcodeScanner.startScan();
+  }
+
+  async stopScan() {
+    document.body.classList.remove('barcode-scanner-active');
+    await BarcodeScanner.removeAllListeners();
+    await BarcodeScanner.stopScan();
+    this.changeDetect.detectChanges();
+  }
 }
